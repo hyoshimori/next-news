1dc83f9fbd16acc51db2d71f91727a3a40e61522
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-03-11 06:05:51 +0100

    flex to each news(index 0 - 5)

--- components/NewsTimeLine.tsx
+++ components/NewsTimeLine.tsx
@@ -1,12 +1,11 @@
 import styles from "./NewsTimeLine.module.css"
 import React from 'react';
-import Trend from "./TheTrend";
+import Article from "./Article";
 
 const NewsTimeLine = () => {
   return (
   <div className={styles.body}>
-    body
-    <Trend />
+    <Article />
   </div>
   )
 }
