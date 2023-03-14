e14a3b08471387e8a2fc9e3de7544bc7b2ca063c
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-03-14 07:23:45 +0100

    flex design wip

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,5 +1,6 @@
 import styles from "./Article.module.css"
 import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 
 import { useNews } from '@/hooks/UseNews';
 
@@ -36,23 +37,39 @@ const Article = () => {
       <div>
         <div className={styles.news__top__wrapper}>
           {news.filter((el, index) => index === 0).map((el, index) =>
-            <div key={el.url} className={styles.news__top}>
-              <p>{el.url}: {index}</p>
-            </div>
+          <a href={el.url} key={el.url}>
+            <div key={el.url} className={styles.news__top__first}>
+              <img src={el.urlToImage} alt="" />
+              <div className={styles.news__top__first__bottom}>
+                <span className={styles.news__top__first__bottom__source__name}>{el.source.name}</span>
+                <span className={styles.news__top__first__bottom__title}>{el.title}</span>
+                <span className={styles.news__top__first__bottom__author}>{el.author}</span>              </div>
+              </div>
+          </a>
           )}
-        <div className={styles.news__top__wrapper__for__five__articles}>
-          {news.filter((el, index) => index >= 1 && index <= 5).map((el, index) =>
-            <div key={el.url} className={styles.news__top}>
-              <p>{el.url}: {index + 1}</p>
-            </div>
-          )}
-        </div>
-        </div>
-        {news.filter((el, index) => index > 5).map((el, index) =>
-          <div key={el.url} className={styles.news}>
-            <span>{el.url}</span>
-            <p>{index + 6}</p>
+          <div className={styles.news__top__wrapper__for__five__articles}>
+            {news.filter((el, index) => index >= 1 && index <= 5).map((el, index) =>
+              <div key={el.url} className={styles.news__top__right}>
+                <span className={styles.news__top__source__name}>{el.source.name}</span>
+                <span className={styles.news__top__title}>{el.title}</span>
+                <span className={styles.news__top__author}>{el.author}</span>
+              </div>
+            )}
           </div>
+        </div>
+        <p style={{ fontWeight: "bold", marginBottom: "20px" }}>The Latest</p>
+        {news.filter((el, index) => index > 5 && index <= 30).map((el, index) =>
+        <a href={el.url} key={el.url}>
+          <div key={el.url} className={styles.news__latest}>
+            <div className={styles.news__top__first__bottom}>
+              <span className={styles.news__latest__source__name}>{el.source.name}</span>
+              <span className={styles.news__latest__title}>{el.title}</span>
+              <span className={styles.news__latest__author}>{el.author}</span>
+            </div>
+            <span className={styles.news__latest__author}>{el.description}</span>
+            <img src={el.urlToImage} alt="" />
+          </div>
+        </a>
         )}
       </div>
     </div>
