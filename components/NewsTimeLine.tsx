bdd7ac424145aeaf1e9582a9e0c058a95e3c84e2
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-03-17 06:54:02 +0100

    text code added

--- components/NewsTimeLine.tsx
+++ components/NewsTimeLine.tsx
@@ -1,11 +1,13 @@
 import styles from "./NewsTimeLine.module.css"
 import React from 'react';
 import Article from "./Article";
+import Footer from "./Footer";
 
 const NewsTimeLine = () => {
   return (
   <div className={styles.body}>
     <Article />
+    <Footer />
   </div>
   )
 }
