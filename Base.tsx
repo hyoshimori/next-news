3d0b1d4767842cbc71cb9ef19abf21dfc1d5abfb
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-03-19 06:51:39 +0100

    my conetxt set up

--- Base.tsx
+++ Base.tsx
@@ -4,10 +4,23 @@ import NewsTimeLine from './components/NewsTimeLine';
 import { createContext } from 'react';
 
 // import * as NewsType from "@/types/News";
+import { Category } from "@/types/Category";
 
 import 'normalize.css';
 
-export const MyContext = createContext('hi')
+interface MyContext {
+  category: Category;
+}
+
+export const MyContext = createContext<MyContext>({
+  category: {
+    MyContext: {
+      country: "us",
+      category: "anime"
+    }
+  }
+});
+
 
 const Base = () => {
   // const newsData: NewsType.News[] = [ /*...*/ ];
