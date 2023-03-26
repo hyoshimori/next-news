6706b8c5eb6bf401c0d40d354edb7a886307cb3d
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-03-27 01:45:52 +0200

    footer margins fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Footer = () => {
   return (
   <div className={styles.body}>
-    <span><a className={styles.attribute} href="/about">About</a></span>
-    <span>News data is provided by <a className={styles.attribute} target="_blank" href="https://developer.nytimes.com/apis">New York Times API</a>.</span>
+    <span>| <a className={styles.attribute} href="/about">About</a> NextNews.</span>
+    <span>| Data by <a className={styles.attribute} target="_blank" href="https://developer.nytimes.com/apis">New York Times API</a>.</span>
   </div>
   )
 }
