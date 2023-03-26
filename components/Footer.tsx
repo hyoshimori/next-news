a42586e332bb0ce687691b139361e20b1131ca45
Hiroto Yoshimori <108004372+hyoshimori@users.noreply.github.com>
2023-03-27 00:31:48 +0200

    Update Footer.tsx
    Footer-attribute updated.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
   <div className={styles.body}>
     <span><a className={styles.attribute} href="/about">About</a></span>
-    <span>News data is provided by <a className={styles.attribute} target="_blank" href="https://newsapi.org/">NewsAPI</a>.</span>
+    <span>News data is provided by <a className={styles.attribute} target="_blank" href="https://developer.nytimes.com/apis">New York Times API</a>.</span>
   </div>
   )
 }
