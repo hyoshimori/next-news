f0aa2ad9a3298da63aa7facbb41a24a6f251136e
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-03-29 02:05:32 +0200

    test code updated

--- pages/_app.test.tsx
+++ pages/_app.test.tsx
@@ -1,6 +1,10 @@
 import { render } from '@testing-library/react'
 import NavLayout from '../layouts/NavLayout'
 
+import { useRouter } from 'next/router';
+
+jest.mock('next/router');
+
 describe('NavLayout', () => {
   it('renders the Nav component', () => {
     const { container } = render(<NavLayout>test content</NavLayout>)
