f9a00c5918ef50d813ad8c4b0c0d177a730903de
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-04-04 20:59:49 +0200

    search file refactored

--- components/Search.tsx
+++ components/Search.tsx
@@ -7,68 +7,15 @@ import { useNews } from '@/hooks/UseNews';
 import { useContext } from "react";
 import { AppContext } from "../pages/_app";
 
+import { removeDuplicates } from "@/utility/newsUtils";
+import { generateRandomPassword } from "@/utility/newsGenerateRandomPassword";
+import { sleep } from "@/utility/newsSleep";
+import { handleNavigation } from "@/utility/newsHandleNavigation";
+// import { arrSort } from "@/utility/newsArrSort"; => WIP
+
 import * as NewsType from "@/types/News";
 import * as AutoCompleteItemType from "@/types/AutoCompleteItem";
 import * as NewsItemCheckType from "@/types/NewsItemCheck";
-
-// A password creating func
-const generateRandomPassword = () => {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_+=[]{}|;:,.<>?/';
-  let result = '';
-  for (let i = 0; i < 16; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
-
-// ms stands for milliseconds
-// setTimeout() set a timer which will excute the resolve in n secound later (returns promise object).
-async function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
-// ********** This is used to remove the duplicates in the arr ********** //
-// first data as an argument looks like this: {uri: 'nyt://article/1f3a4fbd-9514-5xxxxxxxxxxxxxxxxxxxx', url: 'https://www.nytimes.com/2023/03/31/us/xxxxxxxxxxxxxxxxxxxx', id: 1000xxxxxxxxxx, asset_id: 10000xxxxxxxxxx, source: 'New York Times', …}
-// second argument "key" is the url wich looks like this: https://www.nytimes.com/2023/03/31/arts/telxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-const removeDuplicates = (data: NewsItemCheckType.NewsItemCheck[], key: keyof NewsItemCheckType.NewsItemCheck): NewsItemCheckType.NewsItemCheck[] => {
-  // ↓ Pick up only the truthy el
-  return data.filter((item, index, self) => {
-    // ↓ Check if the index of the current el in the loop is the same as the index of the first occurrence of a similar element (based on the key)
-    return index === self.findIndex((otherItem) =>
-      // OhterItems looks like this: {uri: 'nyt://article/1f3a4fbd-9514-5xxxxxxxxxxxxxxxxxxxx', url: 'https://www.nytimes.com/2023/03/31/us/xxxxxxxxxxxxxxxxxxxx', id: 1000xxxxxxxxxx, asset_id: 10000xxxxxxxxxx, source: 'New York Times', …}
-      // OtherItems[key] looks like this: https://www.nytimes.com/2023/03/31/arts/telxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-      otherItem[key] === item[key]
-    );
-  });
-};
-// ********************************************************************** //
-
-// This func wip
-
-// const arrSort = (arr: NewsItemCheckType.NewsItemCheck[]) => {
-//   console.log(arr)
-//   // You must return values with either 1 or -1 or 0 for this custom func
-//   return arr.sort((a, b) => {
-
-//     const abstractA = a.abstract.toLowerCase();
-//     const abstractB = b.abstract.toLowerCase();
-
-//     // Return a negative, zero, or positive value based on the comparison
-//     if (abstractA < abstractB) {
-//       // in case of -1, the el is to be placed before
-//       return -1;
-//     } else if (abstractA > abstractB) {
-//      // in case of 1, the el is to be placed after
-//       return 1;
-//     } else {
-//       // in case of 0, the order of a and b does not matter
-//       return 0;
-//     }
-//   })
-//   return
-// }
-
 
 const Search = () => {
   const [input, setInput] = useState<string | undefined>(undefined);
@@ -80,8 +27,20 @@ const Search = () => {
   const [isForcused, setIsisForcused] = useState(true);
 
   const { axios } = useNews();
-
   const checker: any = []
+
+
+  // This func is used to remove the value in the field
+  const blurringDetector = async () => {
+    // setIsBlurred(input)
+    await sleep(150)
+    setInput('')
+    setIsisForcused(true)
+  }
+
+  const focusDetector = () => {
+    setIsisForcused(false)
+  }
 
 
   // First useEffect to get api call
@@ -119,24 +78,6 @@ const Search = () => {
     }
   }, [input]);
 
-  // This func is used to open the link when suggestions are being clicked
-  const handleNavigation = (url: string) => {
-    if (url) {
-      window.open(url, '_blank', 'noopener,noreferrer');
-    }
-  };
-
-  // This func is used to remove the value in the field
-  const blurringDetector = async () => {
-    // setIsBlurred(input)
-    await sleep(150)
-    setInput('')
-    setIsisForcused(true)
-  }
-
-  const focusDetector = () => {
-    setIsisForcused(false)
-  }
 
   return (
     <div className={styles.body}>
---
+++ utility/newsGenerateRandomPassword.ts
@@ -0,0 +1,9 @@
+// A password creating func
+export const generateRandomPassword = () => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_+=[]{}|;:,.<>?/';
+  let result = '';
+  for (let i = 0; i < 16; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
