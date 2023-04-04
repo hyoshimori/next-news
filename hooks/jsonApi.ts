3fa44833f8d7823e8cf206c5f6a34c4f2aed0de9
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-04-04 20:37:09 +0200

    hooks

--- hooks/jsonApi.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const getCategoryNews = async (category: string) => {
-  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-  const response = await axios.get(`${apiBaseUrl}/${category}`);
-  return response.data;
-};
