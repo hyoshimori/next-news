57442eb857c11ce1f6d10a4b29c84cce8542cefd
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-10-09 22:38:07 +0900

    chore: Cleaned up css and fixed ui break

--- components/main/timeline/NewsTimeLine.tsx
+++ components/main/timeline/NewsTimeLine.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-import { Article, Footer, NewsTimeLineStyles } from "@/components/index";
+import { Article, NewsTimeLineStyles } from "@/components/index";
 
 const NewsTimeLine = () => {
   return (
     <div className={NewsTimeLineStyles.body}>
       <Article />
-      <Footer />
     </div>
   );
 };
